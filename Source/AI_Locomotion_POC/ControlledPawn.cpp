4dc31e5b0133ee8802018cd5d96a4bf9dc9bede5
lukas.hueglin <lukas.hueglin@outlook.com>
2021-11-13 20:30:28 +0100

    4
    created a function to contract a muscle, enabled player input in order to control the muscle

--- Source/AI_Locomotion_POC/ControlledPawn.cpp
+++ Source/AI_Locomotion_POC/ControlledPawn.cpp
@@ -1,7 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "ControlledPawn.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+#include "Engine/SkeletalMeshSocket.h"
+#include "Math/Vector.h"
 
 // Sets default values
 AControlledPawn::AControlledPawn()
@@ -9,13 +13,98 @@ AControlledPawn::AControlledPawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Create capsule component
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	Capsule->SetupAttachment(RootComponent);
+	Capsule->InitCapsuleSize(11.0f, 96.0f);
+	Capsule->SetVisibility(true, true);
+
+	// Create mesh component
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Capsule);
+	Mesh->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
+	Mesh->SetRelativeLocation(FVector(0.0f, 0.0f, -89.0f));
+	Mesh->bCastDynamicShadow = true;
+
+	// Create arrow component
+	Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	Arrow->SetupAttachment(Capsule);
+
+	// Create apring arm component
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(Capsule);
+
+	// Create apring arm component
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+	Camera->bUsePawnControlRotation = true;
+
+	// Initialize float
+	muscleActivation = 0.0f;
 }
 
 // Called when the game starts or when spawned
 void AControlledPawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// Enable Ragdoll
+	GetMesh()->SetAllBodiesBelowSimulatePhysics(FName("pelvis"), true, false);
+	GetMesh()->SetSimulatePhysics(true);
+	GetMesh()->WakeAllRigidBodies();
+	GetMesh()->bBlendPhysics = true;
+
+	// Enable player input
+	EnableInput(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+}
+
+// Called to move the joint between the socket and it's parent in a single axis
+void AControlledPawn::MoveJoint(FName BoneName, EAxis::Type Axis, float force)
+{
+	// Create variables for bone names
+	FName p_Name = GetMesh()->GetParentBone(BoneName);
+	FName c_Name = BoneName;
+
+	// Get Bone Locations
+	FVector p_Location = GetMesh()->GetBoneLocation(p_Name);
+	FVector c_Location = GetMesh()->GetBoneLocation(c_Name);
+
+	// Get normal and forward vectors
+	FVector p_Forward = GetMesh()->GetBoneAxis(p_Name, EAxis::Y);
+	FVector c_Normal = GetMesh()->GetBoneAxis(c_Name, Axis);
+	FVector p_Normal = - (c_Normal - FVector::DotProduct(c_Normal, p_Forward) * p_Forward);
+	p_Normal.Normalize();
+
+	// Get center of mass and vectors from the bone positions to the com
+	FVector CenterOfMass = GetMesh()->GetSkeletalCenterOfMass();
+	FVector p_ToCenterOfMass = p_Location - CenterOfMass;
+	FVector c_ToCenterOfMass = c_Location - CenterOfMass;
+
+	// Get length lever arm of both bones
+	float p_angle = FMath::Acos(FVector::DotProduct(p_ToCenterOfMass, p_Normal) / p_ToCenterOfMass.Size());
+	float p_la = sin(p_angle) * p_ToCenterOfMass.Size();
+	float c_angle = FMath::Acos(FVector::DotProduct(c_ToCenterOfMass, c_Normal) / c_ToCenterOfMass.Size());
+	float c_la = sin(c_angle) * c_ToCenterOfMass.Size();
+
+	// Calculate torques
+	FVector p_Torque = (force * c_la * p_Normal) / p_la;
+	FVector c_Torque = force * c_Normal;
+
+	UKismetSystemLibrary::DrawDebugArrow(Mesh, Mesh->GetBoneTransform(Mesh->GetBoneIndex(FName(p_Name))).GetLocation(), Mesh->GetBoneTransform(Mesh->GetBoneIndex(FName(p_Name))).GetLocation() + p_Torque / force * 100, 1.0f, FLinearColor::Red, 0.0f, 1.0f);
+	UKismetSystemLibrary::DrawDebugArrow(Mesh, Mesh->GetBoneTransform(Mesh->GetBoneIndex(FName(c_Name))).GetLocation(), Mesh->GetBoneTransform(Mesh->GetBoneIndex(FName(c_Name))).GetLocation() + c_Torque / force * 100, 1.0f, FLinearColor::Blue, 0.0f, 1.0f);
+
+	Mesh->AddTorqueInRadians(p_Torque, p_Name, false);
+	Mesh->AddTorqueInRadians(c_Torque, c_Name, false);
+}
+
+void AControlledPawn::ControlMuscleActivation(float Val)
+{
+	muscleActivation = Val;
+}
+
+USkeletalMeshComponent* AControlledPawn::GetMesh()
+{
+	return Mesh;
 }
 
 // Called every frame
@@ -23,6 +112,7 @@ void AControlledPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	MoveJoint(FName("calf_l"), EAxis::Z, muscleActivation*1e5);
 }
 
 // Called to bind functionality to input
@@ -30,5 +120,6 @@ void AControlledPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(FName("MuscleControl"), this, &AControlledPawn::ControlMuscleActivation);
 }
 
--- Source/AI_Locomotion_POC/ControlledPawn.h
+++ Source/AI_Locomotion_POC/ControlledPawn.h
@@ -2,6 +2,12 @@
 
 #pragma once
 
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/ArrowComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "ControlledPawn.generated.h"
@@ -15,9 +21,48 @@ public:
 	// Sets default values for this pawn's properties
 	AControlledPawn();
 
+public:
+	// Capsule component
+	UPROPERTY(VisibleDefaultsOnly, Category = "Root")
+	UCapsuleComponent* Capsule;
+
+	// Mesh component
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	USkeletalMeshComponent* Mesh;
+
+	// Arrow component
+	UPROPERTY(VisibleDefaultsOnly, Category = "Root")
+	UArrowComponent* Arrow;
+
+	// Spring arm component
+	UPROPERTY(VisibleDefaultsOnly, Category = "Camera")
+	USpringArmComponent* SpringArm;
+
+	// Camera component
+	UPROPERTY(VisibleDefaultsOnly, Category = "Camera")
+	UCameraComponent* Camera;
+
+	UPROPERTY(BlueprintReadWrite, Category = "MucleContraction")
+	float muscleActivation;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+protected:
+	/*
+	*	Called to move the joint between the socket and it's parent
+	*	The torque vector is realative to the socket's local space
+	*	Method has to be called every tick if a constant torque is wanted
+	*/
+	void MoveJoint(FName BoneName, EAxis::Type Axis, float force);
+
+	void ControlMuscleActivation(float Val);
+
+public:
+	// Called to get the pawn mesh
+	UFUNCTION()
+	USkeletalMeshComponent* GetMesh();
 
 public:	
 	// Called every frame
